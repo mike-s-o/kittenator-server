13ce43b40c7bb6ae2b350c8b81f7bb9dee30fd3a
mike-s-o <52865435+mike-s-o@users.noreply.github.com>
2019-09-03 17:30:34 +0100

    Add redis and basic query api

--- start.js
+++ start.js
@@ -1,12 +1,27 @@
 require('dotenv').config();
 
+const lodash = require('lodash');
+const Redis = require('ioredis');
+const redis = new Redis(6379, 'localhost');
+
 const express = require('express');
 const PORT = process.env.PORT || 5000;
 
 let server = express();
 
-server.get('/hola', async (req, res) => {
-  res.send('hey there');
+server.get('/tweets-status/:tweets', async (req, res) => {
+  const tweets = validateTweetsIDs(req.params.tweets);
+  const results = await redis.mget(tweets.map(t => 'tweet:' + t));
+  const jsonOutput = lodash.pickBy(lodash.zipObject(tweets, results));
+
+  res.send(JSON.stringify(jsonOutput));
 });
+
+let validateTweetsIDs = param => {
+  return ('' + param)
+    .split(',')
+    .map(id => parseInt(id))
+    .filter(id => id);
+};
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
